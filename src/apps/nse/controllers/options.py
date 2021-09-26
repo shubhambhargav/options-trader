d5cd9c607c399c9847f594135514f2d782b88adc
Shubham Bhargav <s.bhargav003@gmail.com>
2021-09-26 16:35:55 +0530

    Added basic backtester for iron candor

--- src/apps/nse/controllers/options.py
+++ src/apps/nse/controllers/options.py
@@ -15,49 +15,51 @@ FLOAT_FIELDS = set(['strike_price', 'open', 'high', 'low', 'close', 'ltp', 'sett
 
 class OptionsController:
     @staticmethod
-    def get_historical_data(tickersymbol: str, option_type: str, expiry_date: date, from_date: date = None, to_date: date = None) -> List[HistoricalOptionModel]:
+    def get_historical_data(tickersymbol: str, expiry_date: date, option_types: str = ['PE', 'CE'], from_date: date = None, to_date: date = None) -> List[HistoricalOptionModel]:
         if not from_date:
             from_date = expiry_date - timedelta(days=7)
 
         if not to_date:
             to_date = expiry_date
 
-        url = 'https://www1.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?instrumentType=OPTIDX&symbol=%(tickersymbol)s&expiryDate=%(expiry_date)s&optionType=%(option_type)s&strikePrice=&dateRange=&fromDate=%(from_date)s&toDate=%(to_date)s&segmentLink=9&symbolCount=' % {
-            'tickersymbol': tickersymbol,
-            'option_type': option_type,
-            'expiry_date': expiry_date.strftime(EXPIRY_DATE_FORMAT),
-            'from_date': from_date.strftime(FROM_TO_DATE_FORMAT),
-            'to_date': to_date.strftime(FROM_TO_DATE_FORMAT)
-        }
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
-            'Referer': 'https://www1.nseindia.com/products/content/derivatives/equities/historical_fo.htm'
-        }
-
-        response = requests.get(url, headers=headers)
-
-        if response.status_code != 200:
-            raise ValueError('Unexpected response code found: %d, response: %s' % (response.status_code, response.text))
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        csv_content = soup.find('div', {'id': 'csvContentDiv'}).get_text()
-        lines = csv_content.split(':')
         data = []
 
-        headers = lines[0].lower().strip() \
-            .replace('"', '').replace(' ', '_').replace('.', '') \
-            .split(',')
+        for option_type in option_types:
+            url = 'https://www1.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?instrumentType=OPTIDX&symbol=%(tickersymbol)s&expiryDate=%(expiry_date)s&optionType=%(option_type)s&strikePrice=&dateRange=&fromDate=%(from_date)s&toDate=%(to_date)s&segmentLink=9&symbolCount=' % {
+                'tickersymbol': tickersymbol,
+                'option_type': option_type,
+                'expiry_date': expiry_date.strftime(EXPIRY_DATE_FORMAT),
+                'from_date': from_date.strftime(FROM_TO_DATE_FORMAT),
+                'to_date': to_date.strftime(FROM_TO_DATE_FORMAT)
+            }
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+                'Referer': 'https://www1.nseindia.com/products/content/derivatives/equities/historical_fo.htm'
+            }
 
-        for line in lines[1:]:
-            if not line:
-                continue
+            response = requests.get(url, headers=headers)
 
-            content = line.strip().replace('"', '').split(',')
-            content = dict(zip(headers, content))
+            if response.status_code != 200:
+                raise ValueError('Unexpected response code found: %d, response: %s' % (response.status_code, response.text))
 
-            content = dict((k, float(v) if k in FLOAT_FIELDS else v) for k, v in content.items())
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-            data.append(from_dict(data_class=HistoricalOptionModel, data=content))
+            csv_content = soup.find('div', {'id': 'csvContentDiv'}).get_text()
+            lines = csv_content.split(':')
+
+            headers = lines[0].lower().strip() \
+                .replace('"', '').replace(' ', '_').replace('.', '') \
+                .split(',')
+
+            for line in lines[1:]:
+                if not line:
+                    continue
+
+                content = line.strip().replace('"', '').split(',')
+                content = dict(zip(headers, content))
+
+                content = dict((k, float(v) if k in FLOAT_FIELDS else v) for k, v in content.items())
+
+                data.append(from_dict(data_class=HistoricalOptionModel, data=content))
 
         return data
--- src/strategies/iron_candor/controllers.py
+++ src/strategies/iron_candor/controllers.py
@@ -1,6 +1,11 @@
+from datetime import date, timedelta
+
 from PyInquirer import Token, prompt, style_from_dict
 from dacite import from_dict
 
+import src.utilities as Utilities
+from src.apps.nse.controllers.options import OptionsController
+from src.apps.kite.controllers.instruments import InstrumentsController
 from src.strategies.iron_candor.models import ConfigModel
 from src.logger import LOGGER
 
@@ -36,4 +41,116 @@ class BackTester:
         return from_dict(data_class=ConfigModel, data=config)
 
     def run(self):
-        config = self.get_config()
+        # config = self.get_config()
+        config = from_dict(data_class=ConfigModel, data={'tickers': ['BANKNIFTY']})
+
+        ## To be put into a configuration
+        delta_expectation = 5  # in percentage
+
+        start_date = date(2021, 3, 11)
+        end_date = date(2021, 9, 23)
+        iteration_date = start_date
+        tickersymbol = config.tickers[0]
+        lot_size = 40
+        iteration_count = 0
+        max_iteration_count = 20
+        total_expected_pnl = 0
+        total_pnl = 0
+
+        skip_week = [
+            date(2021, 3, 11), date(2021, 3, 18), date(2021, 5, 13), date(2021, 5, 20),
+            date(2021, 8, 19), date(2021, 8, 26)
+        ]
+
+        while iteration_date < end_date:
+            if iteration_date in skip_week:
+                iteration_date += timedelta(days=7)
+
+                continue
+
+            position_date = iteration_date - timedelta(days=7)
+
+            instrument_price = InstrumentsController.get_instrument_price_details(
+                tickersymbol=tickersymbol,
+                date_val=position_date
+            ).close
+
+            print('Price for BANKNIFTY on %s is: %s' % (iteration_date, instrument_price))
+
+            low_sell_price = Utilities.round_nearest(number=instrument_price * (1 - delta_expectation / 100), unit=100)
+            high_sell_price = Utilities.round_nearest(number=instrument_price * (1 + delta_expectation / 100), unit=100)
+
+            options_data = OptionsController.get_historical_data(
+                tickersymbol=tickersymbol,
+                expiry_date=iteration_date,
+                from_date=position_date,
+                to_date=position_date
+            )
+
+            option_strike_price_dict = dict((option.option_type + str(int(option.strike_price)), option) for option in options_data)
+
+            low_sell_option = option_strike_price_dict['PE' + str(low_sell_price)]
+            low_buy_option = option_strike_price_dict['PE' + str(low_sell_price - 100)]
+            high_sell_option = option_strike_price_dict['CE' + str(high_sell_price)]
+            high_buy_option = option_strike_price_dict['CE' + str(high_sell_price + 100)]
+
+            # print(low_buy_option)
+            # print(low_sell_option)
+            # print(high_sell_option)
+            # print(high_buy_option)
+
+            expected_max_profit = (low_sell_option.close + high_sell_option.close - low_buy_option.close - high_buy_option.close) * lot_size
+            total_expected_pnl += expected_max_profit
+
+            print('Expected max profit: %.2f' % expected_max_profit)
+
+            expiry_day_instrument_price = InstrumentsController.get_instrument_price_details(
+                tickersymbol=tickersymbol,
+                date_val=iteration_date
+            ).close
+
+            expiry_options_data = OptionsController.get_historical_data(
+                tickersymbol=tickersymbol,
+                expiry_date=iteration_date,
+                from_date=iteration_date,
+                to_date=iteration_date
+            )
+
+            expiry_option_strike_price_dict = dict((option.option_type + str(int(option.strike_price)), option) for option in expiry_options_data)
+
+            expiry_low_sell_option = expiry_option_strike_price_dict['PE' + str(low_sell_price)]
+            expiry_low_buy_option = expiry_option_strike_price_dict['PE' + str(low_sell_price - 100)]
+            expiry_high_sell_option = expiry_option_strike_price_dict['CE' + str(high_sell_price)]
+            expiry_high_buy_option = expiry_option_strike_price_dict['CE' + str(high_sell_price + 100)]
+
+            pnl = (
+                (low_sell_option.close + high_sell_option.close) -
+                    (expiry_low_sell_option.close + expiry_high_sell_option.close) -
+                (low_buy_option.close + high_buy_option.close) +
+                    (expiry_low_buy_option.close + expiry_high_buy_option.close)
+            ) * lot_size
+
+            # print(expiry_low_buy_option)
+            # print(expiry_low_sell_option)
+            # print(expiry_high_sell_option)
+            # print(expiry_high_buy_option)
+
+            print('Final pnl achieved: %.2f' % pnl)
+
+            total_pnl += pnl
+
+            price_percentage_change = (expiry_day_instrument_price - instrument_price) / instrument_price * 100
+            print('Price change happened: %.2f percent' % price_percentage_change)
+
+            print('----------------------------')
+
+            iteration_date += timedelta(days=7)
+
+            iteration_count += 1
+
+            if iteration_count >= max_iteration_count:
+                break
+
+        print('-------------------')
+        print('Total expected pnl: %.2f' % total_expected_pnl)
+        print('Final pnl achieved: %.2f' % total_pnl)
