823f2b5ee18f338f322821332074d80ddb33f579
Shubham Bhargav <shubham@postman.com>
2022-12-25 16:10:45 -0800

    Added symbol name to ID conversions

--- src/apps/questrade/controllers/symbols.py
+++ src/apps/questrade/controllers/symbols.py
@@ -6,7 +6,7 @@ from typing import List
 from dacite import from_dict
 from src.apps.settings.controllers import ConfigController
 
-from ..models import CandleModel
+from ..models import CandleModel, SymbolModel
 
 
 class SymbolsController:
@@ -33,3 +33,49 @@ class SymbolsController:
             candles.append(from_dict(data_class=CandleModel, data=row))
 
         return candles
+
+    @staticmethod
+    def get_symbols(ids: list = [], names: list = []) -> List[SymbolModel]:
+        if not ids and not names:
+            raise ValueError('Either ids or names must be provided')
+
+        values = ids if ids else names
+
+        response = requests.get(
+            'https://api06.iq.questrade.com/v1/symbols?%(param)s=%(value)s' % {
+                'param': 'ids' if ids else 'names',
+                'value': ','.join([str(val) for val in values])
+            },
+            headers={
+                'Authorization': f'Bearer {ConfigController.get_config().questrade_access_token}'
+            }
+        )
+
+        if response.status_code != 200:
+            raise ValueError('Unexpected response code found: %d, response: %s' % (response.status_code, response.text))
+
+        symbols: List[CandleModel] = []
+
+        for row in response.json()['symbols']:
+            symbols.append(from_dict(data_class=SymbolModel, data=row))
+
+        return symbols
+
+    @staticmethod
+    def get_symbol(id: int = None, name: str = None) -> SymbolModel:
+        if not id and not name:
+            raise ValueError('Either id or name must be provided')
+
+        url = 'https://api06.iq.questrade.com/v1/symbols/%s' % id if id else 'https://api06.iq.questrade.com/v1/symbols?names=%s' % name
+
+        response = requests.get(
+            url,
+            headers={
+                'Authorization': f'Bearer {ConfigController.get_config().questrade_access_token}'
+            }
+        )
+
+        if response.status_code != 200:
+            raise ValueError('Unexpected response code found: %d, response: %s' % (response.status_code, response.text))
+
+        return from_dict(data_class=SymbolModel, data=response.json()['symbols'][0])
--- src/apps/questrade/models/symbols.py
+++ src/apps/questrade/models/symbols.py
@@ -1,7 +1,44 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Optional
+
+from dataclasses_json import config
 
 
 @dataclass
 class SymbolModel:
     symbol: str
     symbolId: int
+    prevDayClosePrice: float
+    highPrice52: float
+    lowPrice52: float
+    averageVol3Months: int
+    averageVol20Days: int
+    outstandingShares: int
+    eps: float
+    pe: float
+    dividend: float
+    # The following field level aliasing did not work for `from_dict` method in dacite
+    # TODO: Figure out the why and fix it
+    yield_value: Optional[float] = field(metadata=config(field_name='yield'))
+    exDate: str
+    marketCap: int
+    tradeUnit: int
+    optionType: Optional[str]
+    optionDurationType: Optional[str]
+    optionRoot: str
+    optionContractDeliverables: dict
+    optionExerciseType: Optional[str]
+    listingExchange: str
+    description: str
+    securityType: str
+    optionExpiryDate: Optional[str]
+    dividendDate: str
+    optionStrikePrice: Optional[str]
+    isTradable: bool
+    isQuotable: bool
+    hasOptions: bool
+    currency: str
+    minTicks: list
+    industrySector: str
+    industryGroup: str
+    industrySubgroup: str
